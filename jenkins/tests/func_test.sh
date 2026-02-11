667e82d726c923a4d72cf33bcf5bbe07263069a8
Jean-Luc Le Floch <jean-luc.lefloch@idemia.com>
2026-02-11 15:46:46 +0100

    update func_test.sh

--- jenkins/tests/func_test.sh
+++ jenkins/tests/func_test.sh
@@ -8,10 +8,10 @@ HOST_URL=$1
 echo "HOST_URL: " $HOST_URL
 
 # *** Test #1: Check the health API call
-curlCommand = "curl -s -o /dev/null -I -w " + "%{http_code}" + "  " + ${HOST_URL} + "/api/v1/health"
+curlCommand = \"curl -s -o /dev/null -I -w \" + \"%{http_code}\" + \" \" + ${HOST_URL} + \"/api/v1/health\"
 responseCode=$(curl -s -o /dev/null -I -w "%{http_code}"  ${HOST_URL}/api/v1/health)
 if [[ ${responseCode} != 200 ]]; then
-    echo "curlCommand: " curl -s -o /dev/null -I -w "%{http_code}"  ${HOST_URL}/api/v1/health
+    echo "curlCommand: $curlCommand"
     echo "Response code: $responseCode"
     echo "*** health API is not running"
     exit 1
