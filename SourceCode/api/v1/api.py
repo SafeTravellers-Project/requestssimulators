3487727fbb2f2945fc6691ff3115d87f5f993236
jllf1967 <124037602+jllf1967@users.noreply.github.com>
2026-03-02 18:31:19 +0100

    Update api.py
    use header instead of cookie to send and receive session id

--- SourceCode/api/v1/api.py
+++ SourceCode/api/v1/api.py
@@ -7,18 +7,25 @@ from uuid import uuid4
 documentCheck_router = APIRouter()
 nextResultKO_router = APIRouter()
 
+def get_or_create_session_id(request: Request, response: Response) -> str:
+    # get session ID from header X-Session-Id.
+    session_id = request.headers.get("X-Session-Id")
+
+    if not session_id:
+        session_id = str(uuid4())
+
+    # put the session id in the response so client pour qu'il puisse le réutiliser
+    response.headers["X-Session-Id"] = session_id
+
+    return session_id
+
 @documentCheck_router.post("/documentCheck")
 async def documentCheck(data: InputData, request: Request, response: Response):
     # get service id
     service_id = os.getenv("SERVICE_ID")
 
     # get session id
-    session_id = request.cookies.get("sid")
-    #print("session_id: " + session_id)
-    if not session_id:
-        # No session → generate or treat as stateless
-        session_id = str(uuid4())
-        response.set_cookie("sid", session_id, httponly=True)
+    session_id = get_or_create_session_id(request, response)
 
     key = f"{service_id}:session:{session_id}:next_result"
 
@@ -39,11 +46,7 @@ async def nextToggleResult(request: Request, response: Response):
     service_id = os.getenv("SERVICE_ID")
 
     # get session id
-    session_id = request.cookies.get("sid")
-    if not session_id:
-        # No session: generate or treat as stateless
-        session_id = str(uuid4())
-        response.set_cookie("sid", session_id, httponly=True)
+    session_id = get_or_create_session_id(request, response)
 
     key = f"{service_id}:session:{session_id}:next_result"
     r = request.app.state.redis
