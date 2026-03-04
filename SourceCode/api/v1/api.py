b36f504acb8d806694747246aed2b53554259b7d
jllf1967 <124037602+jllf1967@users.noreply.github.com>
2026-03-04 11:39:46 +0100

    Update api.py
    2 changes: 
    * session id must be sent with the request and not generated by the server if no present.
    * nextResultKO is not linked to a session only to a service (server)

--- SourceCode/api/v1/api.py
+++ SourceCode/api/v1/api.py
@@ -12,7 +12,9 @@ def get_or_create_session_id(request: Request, response: Response) -> str:
     session_id = request.headers.get("X-Session-Id")
 
     if not session_id:
-        session_id = str(uuid4())
+        #session_id = str(uuid4())
+        session_id = ""
+        return session_id
 
     # put the session id in the response so client pour qu'il puisse le réutiliser
     response.headers["X-Session-Id"] = session_id
@@ -26,8 +28,12 @@ async def documentCheck(data: InputData, request: Request, response: Response):
 
     # get session id
     session_id = get_or_create_session_id(request, response)
+    if session_id == "":
+        return {"status": "error", "msg" : ""}
 
-    key = f"{service_id}:session:{session_id}:next_result"
+    # this request to have KO in the result is for any request for this service
+    # independently of the session id
+    key = f"{service_id}:next_result"
 
     # Use the Redis client from app state (async)
     r = request.app.state.redis
@@ -45,10 +51,10 @@ async def nextToggleResult(request: Request, response: Response):
     # get service id
     service_id = os.getenv("SERVICE_ID")
 
-    # get session id
-    session_id = get_or_create_session_id(request, response)
-
-    key = f"{service_id}:session:{session_id}:next_result"
+    # this request is not linked to a session,
+    # so no session id is needed for the request
+    # the key does not use the session id
+    key = f"{service_id}:next_result"
     r = request.app.state.redis
     # Set flag for this session for 5 minutes
     await r.set(key, "KO", ex=300)
